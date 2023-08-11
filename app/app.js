dab81eddcb740e9c26461bbbeb771fa26c17985c
jungwon <jungwontwo2@naver.com>
2023-08-11 13:50:17 +0900

    bodyParser 추가

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 "use strict";
 //모듈
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 const PORT = 3000;
@@ -12,7 +13,8 @@ const home = require("./src/routes/home");
 app.set("views","src/views");
 app.set("view engine","ejs");
 app.use(express.static(`${__dirname}/src/public`))
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.use("/",home);//use -> 미들웨어를 등록해주는 메소드
 
