6990217891d0afbbb4ff5322588a2e3e29603e98
jungwon <jungwontwo2@naver.com>
2023-08-11 13:38:40 +0900

    돔을 이용하여 html 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,18 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+const id = document.querySelector("#id");
+const psword = document.querySelector("#psword");
+const loginBtn = document.querySelector("button");
+
+console.log(id);
+console.log("Hello");
+
+loginBtn.addEventListener("click",login);
+
+function login(){
+    const req={
+        id : id.value,
+        psword:psword.value,
+    };
+    console.log(req);
+}
