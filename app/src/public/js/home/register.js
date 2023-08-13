4db2d7e6a13c95b717488e75280fb75cbf30eab2
jungwon <jungwontwo2@naver.com>
2023-08-13 23:00:21 +0900

    회원가입 요청 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -1,20 +1,25 @@
 "use strict";
 
 const id = document.querySelector("#id");
+const name = document.querySelector("#name");
 const psword = document.querySelector("#psword");
-const loginBtn = document.querySelector("#button");
+const confirmPsword = document.querySelector("#confirm-psword");
+const registerBtn = document.querySelector("#button");
 
-console.log(id);
+//console.log(id);
 console.log("Hello");
 
-loginBtn.addEventListener("click",login);
+registerBtn.addEventListener("click",register);
 
-function login(){
+function register(){
     const req={
         id : id.value,
+        name: name.value,
         psword:psword.value,
+        confirmPsword:confirmPsword.value,
     };
-    fetch("/login",{
+    console.log(req);
+    fetch("/register",{
         method: "POST",
         headers:{
             "Content-Type":"application/json",
@@ -30,6 +35,6 @@ function login(){
          }
     })
     .catch((err)=>{
-        console.error(("로그인 중 에러 발생"));
+        console.error(("회원가입 중 에러 발생"));
     });
 }
